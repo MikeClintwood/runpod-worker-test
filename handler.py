49247f70e9ad7dea79a9e2741c6fa88601d9b27d
MikeClintwood <158066825+MikeClintwood@users.noreply.github.com>
2026-04-13 19:20:17 +0200

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,1 +1,14 @@
+import runpod
 
+def handler(job):
+    job_input = job.get("input", {})
+
+    print("Job erhalten:", job_input)
+
+    return {
+        "status": "ok",
+        "message": "Worker läuft",
+        "input_received": job_input
+    }
+
+runpod.serverless.start({"handler": handler})
