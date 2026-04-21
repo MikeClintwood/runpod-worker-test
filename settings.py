b62615cafd79fb272954bee5387b8a5e3d0df5cd
MikeClintwood <158066825+MikeClintwood@users.noreply.github.com>
2026-04-21 15:14:52 +0200

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,22 @@
+FORMAT_PRESETS = {
+    "portrait": {
+        "width": 720,
+        "height": 1280
+    },
+    "landscape": {
+        "width": 1280,
+        "height": 720
+    }
+}
+
+VIDEO_SETTINGS = {
+    "fps": 8,
+    "codec": "libx264",
+    "pixel_format": "yuv420p",
+    "crf": 23,
+    "preset": "veryfast"
+}
+
+NETWORK_SETTINGS = {
+    "download_timeout": 10
+}
